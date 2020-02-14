0a427cf41896fe8bbc252f3f8d7cd38823ae73a9
まさきまさき <natumasa2@gmail.com>
2020-02-15 01:13:51 +0900

    育成モードテストデータ追加

---
+++ PowerBattleTraveler/Assets/Code/BreedMode/MainMenu/BreedModeMainMenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 育成モードメインメニュー基盤
+/// </summary>
+public class BreedModeMainMenuController : MonoBehaviour
+{
+    //メニュー種別
+    private enum MenuType
+    {
+        Training,    // 訓練
+        Extra_Training,    // 特別訓練
+        Rest,    // 休息
+        Skill_Up,    // 能力振り分け
+        Option    // 設定
+    }
+
+    //ターン情報
+    [SerializeField]
+    private struct TurnCount{
+        int Month;    //　月
+        int Week;    //　週
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
