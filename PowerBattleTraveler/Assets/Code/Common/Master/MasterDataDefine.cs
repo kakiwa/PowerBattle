9ac194961f76da93e4a570d9e992fe66d2b40caf
masaki-hirano <natumasa2@gmail.com>
2020-04-17 23:06:00 +0900

    マスタデータ取得処理

---
+++ PowerBattleTraveler/Assets/Code/Common/Master/MasterDataDefine.cs
@@ -0,0 +1,75 @@
+﻿
+/// <summary>
+///　マスターデータの構造体定義
+///</summary>
+
+namespace Common.Master
+{
+
+/// <summary>
+///　マス情報（レーン）
+///</summary>
+[System.Serializable]
+public struct BreedFieldLaneMaster
+{
+    public BreedFieldLaneMaster(int id, int fieldId, int lane, int massSize)
+    {
+        this.id = id;
+        this.fieldId = fieldId;
+        this.lane = lane;
+        this.massSize = massSize;
+    }
+
+    //　ID
+    public int id { get; }
+    //　フィールドID
+    public int fieldId { get; }
+    //　レーン番号
+    public int lane { get; }
+    //　マス数
+    public int massSize { get; }
+
+}
+
+/// <summary>
+///　マス情報（マス内部）
+///</summary>
+[System.Serializable]
+public struct BreedFieldMassMaster
+{
+    public BreedFieldMassMaster(int id, int fieldId, int laneId, string image, string text, int massDataNumber, int massDataType, int massDataRarely, int massDataTextureMount)
+    {
+        this.id = id;
+        this.fieldId = fieldId;
+        this.laneId = laneId;
+        this.image = image;
+        this.text = text;
+        this.massDataNumber = massDataNumber;
+        this.massDataType = massDataType;
+        this.massDataRarely = massDataRarely;
+        this.massDataTextureMount = massDataTextureMount;
+    }
+    //　ID
+    int id { get; }
+    //　フィールドID
+    int fieldId { get; }
+    //　レーンID
+    int laneId { get; }
+    //　マス画像
+    public string  image { get; }
+    //　マス表示テキスト
+    public string text { get; }
+    //　マス情報（マス番号）
+    public int massDataNumber { get; }
+    //　マス情報（マスの種類：０＝初期値、１＝バトル、２＝ボスバトル、３＝イベント、４＝宝箱、５＝特殊）
+    public int massDataType { get; }
+    //　マス情報（マスのレアリティ：０＝初期値、１＝ノーマル、２＝レア、３＝スーパーレア、４＝ウルトラレア）
+    public int massDataRarely { get; }
+    //　マス情報（マス表示画像ベース：０＝初期値、１＝草原、２＝荒野　現在は仮設定）
+    public int massDataTextureMount { get; }
+
+    
+}
+
+
+}
---
+++ PowerBattleTraveler/Assets/Code/Common/Master/MasterDataLoader.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.IO;
+
+namespace Common.Master
+{
+public class MasterDataLoader : MonoBehaviour
+{
+    //------------------------------------------------------------------------------------//
+    //　マスタ格納変数定義　：　新規追加のマスタがある場合ここに追加（定義はMasterDataDefine.csで行う）
+    //------------------------------------------------------------------------------------//
+
+    //　マス情報（レーン）
+    BreedFieldLaneMaster[] breedFieldLaneMaster = default;
+
+    //　マス情報（マス内部）
+    BreedFieldMassMaster[] breedFieldMassMaster = default;
+
+    //-----------------------------------------------------------------------//
+    //　データ管理処理
+    //-----------------------------------------------------------------------//
+
+    // GameControllerインスタンスの実体
+    private static MasterDataLoader instance;
+    // GameControllerインスタンスのプロパティーは、実体が存在しないとき（＝初回参照時）実体を探して登録する
+    public static MasterDataLoader Instance
+    {
+        get
+        {
+            //　実体があるかどうか
+            if (instance == null)
+            {
+                //　実体がないので検索して登録
+                instance = GameObject.FindWithTag("MasterDataLoader").GetComponent<MasterDataLoader>();
+            }
+            return instance;
+        }
+        private set
+        {
+            instance = value;
+        }
+    }
+    //初期化
+    private void Awake()
+    {
+        // もしインスタンスが複数存在するなら、自らを破棄する
+        if (this != Instance)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        // 唯一のインスタンスなら、シーン遷移しても残す
+        DontDestroyOnLoad(this.gameObject);
+
+        LoadMaster();
+
+    }
+    //消える時の処理
+    private void OnDestroy()
+    {
+        // 破棄時に、登録した実体の解除を行う
+        if (this == Instance)
+        {
+            instance = null;
+        }
+    }
+
+
+    //-----------------------------------------------------------------------//
+    //　マスタデータ取得処理　：　新規追加のマスタがある場合ここに追加
+    //-----------------------------------------------------------------------//
+
+    ///<summary>
+    ///　マスタデータ単体取得処理
+    ///</summary>
+    public void LoadMaster()
+    {
+        GetBreedFieldLaneMaster();
+
+    }
+
+    ///<summary>
+    ///　マスタデータ全取得処理
+    ///</summary>
+    public void LoadMasterAll()
+    {
+        //　マスタデータの初期化
+        MasterCacheReset();
+
+        //　各種マスタの取得処理
+        GetBreedFieldLaneMaster();
+        GetBreedFieldMassMaster();
+    }
+
+    ///<summary>
+    ///　マスタデータキャッシュ初期化処理
+    ///</summary>
+    public void MasterCacheReset()
+    {
+        breedFieldLaneMaster = default;
+        breedFieldMassMaster = default;
+    }
+
+    ///<summary>
+    ///　マス情報（レーン）マスタ取得
+    ///</summary>
+    void GetBreedFieldLaneMaster()
+    {
+        int i = 0, j;
+        TextAsset csv = Resources.Load("Master/BreedMode/breed_field_lane_mast") as TextAsset;
+        StringReader reader = new StringReader(csv.text);
+        breedFieldLaneMaster = new BreedFieldLaneMaster[reader.Peek()];
+        while (reader.Peek() > -1)
+        {
+            string line = reader.ReadLine();
+            string[] values = line.Split(',');
+            for (j = 0; j < values.Length; ++j)
+            {
+                breedFieldLaneMaster[i] = new BreedFieldLaneMaster(int.Parse(values[0]),int.Parse(values[1]),int.Parse(values[2]),int.Parse(values[3]));
+            }
+            ++i;
+        }
+    }
+
+    ///<summary>
+    ///　マス情報（マス内部）マスタ取得
+    ///</summary>
+    void GetBreedFieldMassMaster()
+    {
+        int i = 0, j;
+        TextAsset csv = Resources.Load("Master/BreedMode/breed_field_mass_mast") as TextAsset;
+        StringReader reader = new StringReader(csv.text);
+        breedFieldMassMaster = new BreedFieldMassMaster[reader.Peek()];
+        while (reader.Peek() > -1)
+        {
+            string line = reader.ReadLine();
+            string[] values = line.Split(',');
+            for (j = 0; j < values.Length; ++j)
+            {
+                breedFieldMassMaster[i] = new BreedFieldMassMaster(int.Parse(values[0]),int.Parse(values[1]),int.Parse(values[2]),values[3],values[4],int.Parse(values[5]),int.Parse(values[6]),int.Parse(values[7]),int.Parse(values[8]));              
+            }
+            ++i;
+        }
+    }
+
+
+
+}
+}
